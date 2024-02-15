b8a5b0e8aa3249f111668b623378f72ceca05c57
balavignesh111 <balavignesh1018@gmail.com>
2024-02-15 09:07:09 +0530

    second container content added

--- script.js
+++ script.js
@@ -4,14 +4,12 @@ const menuButton = document.querySelector('.menu-btn');
 const closeButton = document.querySelector('.close-btn');
 const navContainer = document.getElementById('nav-list-container');
 
-console.log(document.getElementById("nav-list-container"));
 menuButton.addEventListener('click',()=>{
-  navContainer.style.display = "flex"
   navContainer.style.marginLeft = "0rem";
 
 })
 
 
 closeButton.addEventListener('click',()=>{
-  navContainer.style.display = "none"
+  navContainer.style.marginLeft = "-50%"; 
 })
